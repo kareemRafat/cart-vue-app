fae6763a673a55d6cc16f5b50f50849b1a5905ba
= <kareem.force@gmail.com>
2022-12-10 22:29:46 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,77 +1,77 @@
 
 const app = Vue.createApp({
 
-	data : () => ({
-		products : products , // from products.js file
-		isCartVisible : false ,
-		selectedCategory : 'all' ,
-		cart  : {
-			items : [] // [{product , quantity}]
+	data: () => ({
+		products: products, // from products.js file
+		isCartVisible: false,
+		selectedCategory: 'all',
+		cart: {
+			items: [] // [{product , quantity}]
 		}
-	}) ,
+	}),
 
-	methods : {
+	methods: {
 
-		styleInStock(product){
-			if (product.stock > 10 ) {
+		styleInStock(product) {
+			if (product.stock > 10) {
 				return 'text-success';
 			} else if (product.stock == 0) {
 				return 'text-danger';
 			} else {
 				return 'text-warning';
 			}
-		} ,
+		},
 
-		sortProductByCateogry(){
+		sortProductByCateogry() {
 			if (this.selectedCategory == 'all') {
-				return this.products ;
+				return this.products;
 			} else {
 				return this.products.filter(product => product.category == this.selectedCategory)
 			}
 		},
 
-		addToCart(product){
+		addToCart(product) {
 			!this.checkIfExist(product) ?
-			this.addProductToCart(product) : 
-			this.updateProductQuan(product) ;
+				this.addProductToCart(product) :
+				this.updateProductQuan(product);
 		},
 
-		checkIfExist(product){
+		checkIfExist(product) {
 			// return true if product found in cart items
-			return this.cart.items.some(item => item.product.id == product.id )
-		} ,
+			return this.cart.items.some(item => item.product.id == product.id)
+		},
 
-		addProductToCart(product){
-			this.cart.items.push({product ,quantity : 1});
-			product.stock-- ;
-		} ,
+		addProductToCart(product) {
+			this.cart.items.push({ product, quantity: 1 });
+			product.stock--;
+		},
 
-		updateProductQuan(product){
-			this.cart.items.find(item => item.product.id == product.id).quantity++ ;
-			product.stock-- ;
-		} , 
+		updateProductQuan(product) {
+			this.cart.items.find(item => item.product.id == product.id).quantity++;
+			product.stock--;
+		},
 
-		getTotalPrice(){
+		getTotalPrice() {
 			return this.cart.items
-							.map(item => item.product.price * item.quantity)
-							.reduce((a,b) => a + b);
-		} ,
+				.map(item => item.product.price * item.quantity)
+				.reduce((a, b) => a + b);
+		},
 
-		getCategory(){
+		getCategory() {
 			// get all unique categories from products array
-			var categories =  this.products.map(product => product.category);
+			var categories = this.products.map(product => product.category);
 			return new Set(categories);
 		},
-		onChange(event){
+		onChange(event) {
 			console.log(event.target.value);
-			this.selectedCategory = event.target.value ;
+			this.selectedCategory = event.target.value;
 		}
 
 
 	},
 
 	mounted() {
-		
+
 	}
 
 
